0699b45a8bd8629e7d839534b30c36640a009035
robertsbaer <robertsbaer@yahoo.com>
2019-12-20 12:37:07 -0500

    done and working

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const app = express()
+const bookmarkRoutes = require("./lib/routes/bookmarks")
 
+
+app.listen(8080, () => console.log('port 8080 working'))
+
+
+app.use("/", bookmarkRoutes)
